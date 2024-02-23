8d5e0a9b4e1ff5afc2eb5d08984aedb5329e6cd2
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-02-23 22:35:24 +0200

    zombie

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -2,38 +2,40 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
-
+/**
+ *infinite_while - provides some delay
+ *Return: int
+ */
 int infinite_while(void)
 {
-    while (1)
-    {
-        sleep(1);
-    }
-    return (0);
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
 }
-int main() 
+/**
+ *main - main entry for program
+ *Return: int 0 on success
+ */
+int main(void)
 {
-    pid_t child_pid; // Fork returns process ID
-    int i;
+	pid_t child_pid;
+	int i;
 
-    for (i = 0; i < 5; i++)
-    {
-        child_pid = fork();
-        if (child_pid > 0) 
-        {
-        // Parent process
-
-         sleep(1);
-
-        }
-        else 
-        {
-        // Child process
-        
-        exit(0);    // Child exits
-        
-        }
-    }
-    infinite_while();
-    return (0);
+	for (i = 0; i < 5; i++)
+	{
+		child_pid = fork();
+		if (child_pid > 0)
+		{
+			/*parent*/
+			sleep(1);
+		}
+		else
+		{
+			exit(0);
+		}
+	}
+	infinite_while();
+	return (0);
 }
