953a63a466930f9315b217c4b1dd826bc0a928e6
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-06-04 23:03:34 +0300

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 def number_of_subscribers(subreddit):
     """function that returns total subscribers in subreddit"""
 
-    headers = {"User-Agent": 'Google_chrome/125'}
+    headers = {"User-Agent": 'MyPythonScript/1.0'}
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
 
     res = requests.get(url, headers=headers)
