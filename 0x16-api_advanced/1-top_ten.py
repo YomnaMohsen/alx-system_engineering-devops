086d0defcca50dac3a5f2b36e409dc192ff4ed1e
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-06-05 19:49:34 +0300

    t 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,14 +5,14 @@ import requests
 
 def top_ten(subreddit):
     """lists top_ten titles"""
-    headers = {"User-Agent": 'MyPythonScript/1.0'}
+    headers = {"User-Agent": 'MyPythonCCode/1.0'}
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
 
-    res = requests.get(url, headers=headers)
+    res = requests.get(url, headers=headers, allow_redirects=False)
 
     if res.status_code == 200:
-        top_ten = res.json().get("data").get("childeren")
-        if top_ten is None:
+        top_ten = res.json().get("data").get("children")
+        if not top_ten:
             print("None")
         else:
             for top in top_ten:
