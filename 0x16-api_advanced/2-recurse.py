6e20df5fac7b66e681169b73bfdfcb687f18eccb
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-06-06 23:20:06 +0300

    task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,29 +3,24 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[], page=1, limit=100):
+def recurse(subreddit, hot_list=[], after=""):
     """lists titles using pagination"""
-    if len(hot_list) > limit:
-        return hot_list
-    
-    headers = {"User-Agent": 'Mozilla'}
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    query_param = {"page": page}
 
-    res = requests.get(url, params=query_param, headers=headers)
+    headers = {"User-Agent": 'Yomna is fine'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    query_param = {"after": after}
 
-    if res.status_code == 404:
+    res = requests.get(url, params=query_param, headers=headers,
+                       allow_redirects=False)
+    if res.status_code == 200:
+        after_that = res.json().get("data").get("after")
+
+        if after_that is not None:
+            after = after_that
+            recurse(subreddit, hot_list, after)
+        articles = res.json().get("data").get("children")
+        for p in articles:
+            hot_list.append(p.get("data").get("title"))
+            return hot_list
+    else:    
         return None
-    
-    artc = res.json()
-    
-    if "data" in artc and "children" in artc["data"]:
-        articles = artc.get("data").get("children")
-
-    for p in articles:
-        hot_list.append(p.get("data").get("title"))
-
-    if "data" in artc and "after" in artc["data"]:
-        page += 1
-        recurse(subreddit, hot_list, page)
-    return hot_list
