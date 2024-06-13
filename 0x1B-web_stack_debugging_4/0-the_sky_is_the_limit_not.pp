b35e8e1bdd671de7919b490d892d44f71bdc1a4f
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-06-14 00:45:58 +0300

    t 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,11 @@
 # increase unlimit
 exec {'fix-nginx':
-    command => 'sed -i s/15/4096/ /etc/default/nginx',
+    command => 'sed -i "s/15/4096/" /etc/default/nginx',
     path    => 'usr/local/bin/:/bin/'
 }
 
 # Restart nginx
-~>exec {'restart':
-    command => 'nginx restart'
+exec {'restart':
+    command => '/etc/init.d/nginx restart'
     path    => '/etc/init.d/'
 }
